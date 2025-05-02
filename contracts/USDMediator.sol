88f745821585bb44f723232469457195f231b5c1
ChristianJJones <christian.j.jones@outlook.com>
2025-05-02 09:07:10 -0500

    Update USDMediator.sol

--- contracts/USDMediator.sol
+++ contracts/USDMediator.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDMediator is ERC20, Ownable {
+    constructor() ERC20("USDMediator", "USD") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function transferUSD(address to, uint256 amount) external {
+        _transfer(msg.sender, to, amount);
+    }
+}
